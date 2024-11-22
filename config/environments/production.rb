2c05b1fe4d8d570f58f9911d1e3961b605c8cf8a
tenpa5460 <yamasaki5460@gmail.com>
2024-11-22 20:49:27 +0900

    Update production.rb to set travel_app host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sample-app-5qby.onrender.com'
+  host = 'travel-app-x58z.onrender.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :port           => 587,
